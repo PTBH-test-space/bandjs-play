bd3e479616f65c77b9366d01fe3398d0bceea104
hoangphuc <hoangphucnb97@gmail.com>
2023-08-31 14:18:05 +0700

    Update script

--- src/upload_data_source.ts
+++ src/upload_data_source.ts
@@ -1,5 +1,5 @@
 import {Client, Coin, Fee, Wallet} from '@bandprotocol/bandchain.js';
-import {buildTransaction, createCreateOracleScriptRequest, executeTxn} from './utils';
+import {buildTransaction, createCreateDataSourceRequest, executeTxn} from './utils';
 
 const gpcUrl = 'https://laozi-testnet6.bandchain.org/grpc-web';
 
@@ -8,24 +8,26 @@ const privateKey = Wallet.PrivateKey.fromMnemonic(
   'winter nest benefit zoo team use blanket security card digital stairs often dish ribbon cluster bottom alley hat shaft mask please boss cancel month'
 );
 
+const pubKey = privateKey.toPubkey();
+const sender = pubKey.toAddress().toAccBech32();
+
 (async () => {
-  const sender = privateKey.toPubkey().toAddress().toAccBech32();
-  const msg = await createCreateOracleScriptRequest(
-    'hello_oracle_script.wasm',
-    'HelloWorld',
+  const coin = new Coin();
+  coin.setDenom('uband');
+  coin.setAmount('50000');
+  const msg = await createCreateDataSourceRequest(
+    "gold_price.py",
     sender,
     sender,
-    '{repeat:u64}/{response:string}',
-    'https://ipfs.io/ipfs/QmSSrgJ6QuFDJHyC2SyTgnHKRBhPdLHUD2tJJ86xejrCfn', // source code url
-    '',
+    sender,
+    coin,
+    "",
   );
 
-  const feeCoin = new Coin();
-  feeCoin.setDenom('uband');
-  feeCoin.setAmount('10000');
   const fee = new Fee();
-  fee.setAmountList([feeCoin]);
-  fee.setGasLimit(700000);
+  fee.setAmountList([coin]);
+  fee.setGasLimit(80000);
+
   const txn = await buildTransaction(
     client,
     msg,
@@ -33,10 +35,6 @@ const privateKey = Wallet.PrivateKey.fromMnemonic(
     sender,
   );
 
-  const response = await executeTxn(
-    txn,
-    privateKey,
-    client,
-  );
+  const response = await executeTxn(txn, privateKey, client);
   console.log(response);
 })()
